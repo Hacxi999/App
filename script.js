ab3f04673e6e61f43c85c24713e64e4305819243
Hacxi999 <zapafix49@gmail.com>
2025-02-13 21:52:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('.optimize-btn').addEventListener('click', function() {
+    alert('Otimização aplicada ao Free Fire!');
+});
+
+document.querySelector('.calibrate-btn').addEventListener('click', function() {
+    alert('Tela calibrada com sucesso!');
+});
+
+document.querySelector('.delete-btn').addEventListener('click', function() {
+    alert('Cache deletado!');
+});
